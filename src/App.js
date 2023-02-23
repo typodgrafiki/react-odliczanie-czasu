cfbf72fdd5ef69f6651d6ec1246626171d8a1ea6
typodgrafiki <grzegorz.kocik.pn@gmail.com>
2023-02-23 18:57:19 +0100

    up

--- src/App.js
+++ src/App.js
@@ -3,22 +3,59 @@ import { Vacation, Footer } from './components';
 import today from './components/time/Time';
 import './App.css';
 
-const App = () => {
-    
-    return (
-        <div className='App'>
-            <Vacation 
-                days="0" 
-                hours="0" 
-                minutes="0" 
-                seconds="0" 
-            />
 
-            <Footer 
-                today={today}
-            />
-        </div>
-    )
+let currentDate = new Date();   
+const currentYear = (currentDate.getYear() + 1900);
+const nyDate = new Date(`14 Apr ${currentYear} 17:00:00`);
+
+const updateTime = (thisBind) => {
+        
+    currentDate = new Date();   
+    const totalSeconds = ((nyDate - currentDate) / 1000);    
+    const toSeconds = Math.floor(totalSeconds % 60);
+    const toMinutes = Math.floor((totalSeconds / 60) % 60);
+    const toHours = Math.floor((totalSeconds / 3600) % 24);
+    const toDays = Math.floor((totalSeconds / 3600 / 24));
+    //const months = Math.floor((totalSeconds / 3600 / 24 / 30) % 12);   
+    
+    thisBind.setState({
+        days: addZero(toDays),
+        hours: addZero(toHours),
+        minutes: addZero(toMinutes),
+        seconds: addZero(toSeconds)
+    })    
+    
+    setTimeout(() => {
+        updateTime(thisBind);
+    }, 1000);
+    
+}
+
+function addZero(el) {
+    return (el < 10) ? '0' + el : el;
+}
+
+class App extends React.Component {
+    
+    state = {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+    }
+    
+    componentDidMount() {
+        updateTime(this);
+    }
+    
+    render() {
+        return (
+            <div className='App'>
+                <Vacation timeTo={this.state} />
+                <Footer today={today} />
+            </div>
+        )
+    }    
 }
 
 export default App
--- src/components/vacation/Vacation.jsx
+++ src/components/vacation/Vacation.jsx
@@ -1,70 +1,34 @@
 import React from 'react';
 import './vacation.css';
 
-const Vacation = ({days, hours, minutes, seconds}) => {
-    
-    let currentDate = new Date();   
-    const currentYear = (currentDate.getYear() + 1900);
-    const nyDate = new Date(`14 Apr ${currentYear} 17:00:00`);
-
-    function count(toDate) {
-
-        currentDate = new Date();   
-    
-        const totalSeconds = ((toDate - currentDate) / 1000);    
-        const seconds = Math.floor(totalSeconds % 60);
-        const minutes = Math.floor((totalSeconds / 60) % 60);
-        const hours = Math.floor((totalSeconds / 3600) % 24);
-        const days = Math.floor((totalSeconds / 3600 / 24));
-        //const months = Math.floor((totalSeconds / 3600 / 24 / 30) % 12);
-        
-        let daysEl = document.querySelector("#newYear .days");   
-        let hoursEl = document.querySelector("#newYear .hours");    
-        let minutesEl = document.querySelector("#newYear .minutes");    
-        let secondsEl = document.querySelector("#newYear .seconds");    
-        
-        // daysEl.innerHTML = addZero(days);
-        // hoursEl.innerHTML = addZero(hours);
-        // minutesEl.innerHTML = addZero(minutes);
-        // secondsEl.innerHTML = addZero(seconds);
-        
-        // console.log(days, hours, minutes, seconds);
-
-    }
-
-    function addZero(el) {
-        return (el < 10) ? '0' + el : el;
-    }
-    
-    // count(nyDate);
-    
-    setInterval(() => {
-        count(nyDate);
-    }, 1000);
-    
+const Vacation = ({ timeTo }) => {
     return (
         <div id="newYear" className="section">
-            <h1>Do Islandii zostało:</h1>
+            <div>
+                <h1>Iceland</h1>
+                <h2>Do tripa pozostało:</h2>
+            </div>
             <ul>
                 <li>
-                    <span className="days big">{days}</span>
+                    <span className="days big">{timeTo.days}</span>
                     <span>dni</span>
                 </li>
                 <li>
-                    <span className="hours big">{hours}</span>
+                    <span className="hours big">{timeTo.hours}</span>
                     <span>godzin</span>
                 </li>
                 <li>
-                    <span className="minutes big">{minutes}</span>
+                    <span className="minutes big">{timeTo.minutes}</span>
                     <span>minut</span>
                 </li>
                 <li>
-                    <span className="seconds big">{seconds}</span>
+                    <span className="seconds big">{timeTo.seconds}</span>
                     <span>sekund</span>
                 </li>
             </ul>
         </div>
     )
 }
+
 
 export default Vacation
