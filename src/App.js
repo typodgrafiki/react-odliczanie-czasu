3daf3d19789495bc6da3c8775ef4211eeb6fd818
typodgrafiki <53211722+typodgrafiki@users.noreply.github.com>
2023-03-01 14:43:09 +0100

    up

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import './App.css';
 
 let currentDate = new Date();   
 const currentYear = (currentDate.getYear() + 1900);
-const nyDate = new Date(`14 Apr ${currentYear} 17:00:00`);
+const nyDate = new Date(`15 Apr ${currentYear} 16:50:00`);
+const nyDate2 = new Date(`15 Apr ${currentYear} 11:35:00`);
 
 const updateTime = (thisBind) => {
         
@@ -16,13 +17,24 @@ const updateTime = (thisBind) => {
     const toMinutes = Math.floor((totalSeconds / 60) % 60);
     const toHours = Math.floor((totalSeconds / 3600) % 24);
     const toDays = Math.floor((totalSeconds / 3600 / 24));
+    
+    const totalSeconds2 = ((nyDate2 - currentDate) / 1000);    
+    const toSeconds2 = Math.floor(totalSeconds2 % 60);
+    const toMinutes2 = Math.floor((totalSeconds2 / 60) % 60);
+    const toHours2 = Math.floor((totalSeconds2 / 3600) % 24);
+    const toDays2 = Math.floor((totalSeconds2 / 3600 / 24));
+    
     //const months = Math.floor((totalSeconds / 3600 / 24 / 30) % 12);   
     
     thisBind.setState({
         days: addZero(toDays),
         hours: addZero(toHours),
         minutes: addZero(toMinutes),
-        seconds: addZero(toSeconds)
+        seconds: addZero(toSeconds),
+        days2: addZero(toDays2),
+        hours2: addZero(toHours2),
+        minutes2: addZero(toMinutes2),
+        seconds2: addZero(toSeconds2)
     })    
     
     setTimeout(() => {
@@ -41,7 +53,11 @@ class App extends React.Component {
         days: 0,
         hours: 0,
         minutes: 0,
-        seconds: 0
+        seconds: 0,
+        days2: 0,
+        hours2: 0,
+        minutes2: 0,
+        seconds2: 0
     }
     
     componentDidMount() {
--- src/components/vacation/Vacation.jsx
+++ src/components/vacation/Vacation.jsx
@@ -26,6 +26,27 @@ const Vacation = ({ timeTo }) => {
                     <span>sekund</span>
                 </li>
             </ul>
+            <div>
+                <h2 className="white">Kowalce pozostało:</h2>
+                <ul className="small">
+                    <li>
+                        <span className="days big">{timeTo.days2}</span>
+                        <span>dni</span>
+                    </li>
+                    <li>
+                        <span className="hours big">{timeTo.hours2}</span>
+                        <span>godzin</span>
+                    </li>
+                    <li>
+                        <span className="minutes big">{timeTo.minutes2}</span>
+                        <span>minut</span>
+                    </li>
+                    <li>
+                        <span className="seconds big">{timeTo.seconds2}</span>
+                        <span>sekund</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
